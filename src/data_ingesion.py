0230ea7b13779fa312d2aa641f615f87755e207e
vinaykaladeep <vinaykaladeep1@gmail.com>
2025-11-19 23:58:03 +0530

    1st commit after adding experiments & src files

--- src/data_ingesion.py
+++ src/data_ingesion.py
@@ -89,7 +89,7 @@ def main():
         # params = load_params(params_path='params.yaml')
         # test_size = params['data_ingestion']['test_size']
         test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
+        data_path = 'https://raw.githubusercontent.com/vinaykaladeep/MLOPS-ML-Pipeline/refs/heads/main/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
